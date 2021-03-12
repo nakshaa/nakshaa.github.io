6000d0f2058cf511199e8d482633bb65b84a9ed5
nakshaa <80470230+nakshaa@users.noreply.github.com>
2021-03-12 12:01:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const tileLayer = L.tileLayer(tileURL, {attribution});
 tileLayer.addTo(mymap);
 
 const marker = L.marker([26.79643970762935, 82.1943725566498]);
-marker.bindPopup("<h2>Shree Raam Janmabhoomi Mandir</h2>");
+marker.bindPopup("<div>
+                 <img src="raam-mandir.jpg">
+                 <h2>Shree Raam Janmabhoomi Mandir</h2>
+                 </div>");
 
 marker.addTo(mymap)
