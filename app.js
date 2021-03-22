f484dab18ed4f4588c58363df0f638a613c1414b
nakshaa <80470230+nakshaa@users.noreply.github.com>
2021-03-22 10:24:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mymap = L.map('map').setView([26.796976001970535, 82.19480171008078], 15);
 
 const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by Pravinkumar Singh';
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by Pravinkumar Singh<a href="https://www.github.com/nakshaa/nakshaa.github.io">Github</a>';
 
 const tileLayer = L.tileLayer(tileURL, {attribution});
 
